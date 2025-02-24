63e805931c3ec2f0f8a7aed6615c3a5938e59597
Tymofii Kononenko <timajw95@gmail.com>
2025-02-24 23:43:14 +0100

    666

--- main.js
+++
@@ -1,8 +0,0 @@
-import './js/header';
-import './js/modal';
-import './js/worktogether';
-import './js/covers';
-import './js/projects';
-import './js/aboutme';
-import './js/faq';
-import './js/hero';
